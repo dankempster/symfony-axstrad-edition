854f451f5c7704516f4eeafe6bc1bbadc8d141c7
Dan Kempster <dev@dankempster.co.uk>
2015-01-01 16:41:55 +0000

    Install DoctrineMigrationsBundle
    Also change dependancy stability requirement to dev, but prefer stable releases.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,9 @@ class AppKernel extends Kernel
 
             // Our App
             new AppBundle\AppBundle(),
+
+            // Deployment tools
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
